b34928a39359c0af817cd98e63c94556dec8bede
Ershad <aliershad056@gmail.com>
2024-12-15 20:38:00 +0530

    Initial commit of my MERN project Food-backend

---
+++ backend/api/src/config/db.js
@@ -0,0 +1,28 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const mongoose = require("mongoose")
+
+const uri = "mongodb+srv://rkhan01aa:Ershad_Ali_10@cluster0.ihbye.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+//"mongodb+srv://ashokzarmariya90:V6iwPHCeWMMGvKaW@cluster0.hgjvduv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function connectToDB() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err);
+  }
+  return mongoose.connect(uri)
+}
+
+module.exports = connectToDB;
+
+
